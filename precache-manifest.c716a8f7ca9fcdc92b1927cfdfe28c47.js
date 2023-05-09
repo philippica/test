f5c157ebae710b8bd6cb515097c74e1cc87c0bad
Yuanjie Pu <yuapu@microsoft.com>
2023-05-09 20:59:08 +0800

    Signed-off-by: Yuanjie Pu <yuapu@microsoft.com>

---
+++ precache-manifest.c716a8f7ca9fcdc92b1927cfdfe28c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbb4411e39fc3ca42c91eb366c58b3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a37a736bfa5f3312ac",
+    "url": "/static/css/main.3c11c83b.chunk.css"
+  },
+  {
+    "revision": "20a26c4d2cdf7132dfb3",
+    "url": "/static/js/2.3430a576.chunk.js"
+  },
+  {
+    "revision": "a5a37a736bfa5f3312ac",
+    "url": "/static/js/main.7d0b1f00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
